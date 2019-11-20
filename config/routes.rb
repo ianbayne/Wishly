6f5ffc736212226fbb9f3275165833500ec7159b
Ian Bayne <ian@tacchistudios.com>
2019-11-20 21:38:11 +0900

    new: add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For devise, ensure you have defined root_url to something
+  # root to: 'home#index'
 end
