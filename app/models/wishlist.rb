5a39375ccaef074de2788e3981a67e26eac1fe5f
Ian Bayne <ian@tacchistudios.com>
2019-12-10 15:21:45 +0900

    change: use form object for creating wishlists

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,8 @@
 class Wishlist < ApplicationRecord
   belongs_to :user, touch: true
-  has_many :wishlist_items
+  has_many :wishlist_items, dependent: :destroy
+
+  accepts_nested_attributes_for :wishlist_items
+
+  validates :title, presence: true
 end
