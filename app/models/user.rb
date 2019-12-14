657f66d7eafdc6bc6eace2b6068a9b7243e67756
Ian Bayne <ian@tacchistudios.com>
2019-12-14 13:29:39 +0900

    change: destroy wishlist if user destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :wishlist
+  has_one :wishlist, dependent: :destroy
 
   def phone_number
     PhoneNumber.new(
