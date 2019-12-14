5fabbedc05ce6d5807db86c0bf1825dc7f075d49
Ian Bayne <ian@tacchistudios.com>
2019-12-14 13:29:52 +0900

    new: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Set up Ian
+ian = User.create!(
+  first_name: 'Ian',
+  last_name:  'Bayne',
+  email:      'ian@example.com',
+  password:   'password'
+)
+
+ian.create_wishlist!(title: "Ian's Christmas Wishlist")
+
+5.times do |i|
+  ian.wishlist.wishlist_items.create!(name: "Item ##{i}")
+end
+
+# Set up Holly
+
+holly = User.create!(
+  first_name: 'Holly',
+  last_name:  'Bayne',
+  email:      'holly@example.com',
+  password:   'password'
+)
+
+holly.create_wishlist!(title: "Holly's Birthday list")
+
+5.times do |i|
+  holly.wishlist.wishlist_items.create!(name: "Item ##{i}")
+end
