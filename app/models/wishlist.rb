d3863af4da2a923c0e96ce08a0a28a5a18838a32
Ian Bayne <ian@tacchistudios.com>
2019-12-14 13:48:26 +0900

    change: specifically order wishlist items be created_at asc

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,6 +1,8 @@
 class Wishlist < ApplicationRecord
   belongs_to :user, touch: true
-  has_many :wishlist_items, dependent: :destroy
+  has_many :wishlist_items,
+           -> { order(created_at: :asc) },
+           dependent: :destroy
 
   validates :title, presence: true
 end
