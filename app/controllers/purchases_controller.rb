4cb9fe308891166eba64d0f7ec894ee74104af55
Ian Bayne <ian@tacchistudios.com>
2020-07-05 21:41:53 +0900

    fix: possible race condition when two invitees purchase same item

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,14 +2,21 @@ class PurchasesController < ApplicationController
   def create
     current_user   = User.find(params[:user_id])
     @wishlist_item = WishlistItem.find(params[:id])
-    purchase       = Purchase.new(user: current_user, wishlist_item: @wishlist_item)
+    purchase       = Purchase.new(
+                       user: current_user, wishlist_item: @wishlist_item
+                     )
 
     respond_to do |format|
-      if @wishlist_item.wishlist.owner != current_user && purchase.save
+      if @wishlist_item.wishlist.owner != current_user &&
+        !@wishlist_item.reload.purchased? &&
+        purchase.save
         format.js { flash.now[:notice] = 'Item purchased!' }
       else
-        format.js do
-          flash.now[:alert] = 'Something went wrong. Unable to purchase item.'
+        format.html do
+          redirect_to(
+            wishlist_path(@wishlist_item.wishlist, user_id: current_user),
+            alert: 'Something went wrong. Unable to purchase item.'
+          )
         end
       end
     end
