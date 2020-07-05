17b1e00377f5dbf3d56c5d4fa2d12140b86578ee
Ian Bayne <ian@tacchistudios.com>
2020-07-05 21:13:35 +0900

    change: allow wishlists to be created with one invitee and one item

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -4,7 +4,13 @@ class Wishlist < ApplicationRecord
   has_many   :wishlist_invitees
   has_many   :invitees, through: :wishlist_invitees, source: :user
 
-  accepts_nested_attributes_for :wishlist_items, :owner, :invitees
+  accepts_nested_attributes_for :owner
+  accepts_nested_attributes_for :wishlist_items,
+                                :invitees,
+                                reject_if: :all_blank
 
   validates :title, presence: true
+  validates :wishlist_items,
+            :invitees,
+            length: { minimum: 1 }
 end
--- spec/models/wishlist_spec.rb
+++ spec/models/wishlist_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Wishlist, type: :model do
-  let(:wishlist) { Wishlist.new }
-  let(:user)     { User.new(email: 'user@example.com') }
+  let(:wishlist)      { Wishlist.new }
+  let(:user)          { User.new(email: 'owner@example.com') }
+  let(:wishlist_item) { WishlistItem.new(name: 'Item') }
+  let(:invitee)       { User.new(email: 'invitee@example.com') }
 
-  it 'is valid with a title and an owner' do
+  it 'is valid with a title, an owner, an item, and an invitee' do
     wishlist.title = 'New wishlist'
     wishlist.owner = user
+    wishlist.wishlist_items << wishlist_item
+    wishlist.invitees << invitee
     wishlist.save
 
     expect(wishlist).to be_valid
@@ -14,6 +18,8 @@ RSpec.describe Wishlist, type: :model do
 
   it 'is invalid without a title' do
     wishlist.owner = user
+    wishlist.wishlist_items << wishlist_item
+    wishlist.invitees << invitee
     wishlist.save
 
     expect(wishlist).not_to be_valid
@@ -21,6 +27,26 @@ RSpec.describe Wishlist, type: :model do
 
   it 'is invalid without a user' do
     wishlist.title = 'New wishlist'
+    wishlist.wishlist_items << wishlist_item
+    wishlist.invitees << invitee
+    wishlist.save
+
+    expect(wishlist).not_to be_valid
+  end
+
+  it 'is invalid without at least one wishlist item' do
+    wishlist.title = 'New wishlist'
+    wishlist.owner = user
+    wishlist.invitees << invitee
+    wishlist.save
+
+    expect(wishlist).not_to be_valid
+  end
+
+  it 'is invalid without at least one invitee' do
+    wishlist.title = 'New wishlist'
+    wishlist.owner = user
+    wishlist.wishlist_items << wishlist_item
     wishlist.save
 
     expect(wishlist).not_to be_valid
