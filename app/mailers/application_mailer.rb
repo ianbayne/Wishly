3baa1ac63b9d4d0caf9b6c750822f2debb4afb24
Ian Bayne <ian@tacchistudios.com>
2020-09-01 21:35:28 +0900

    refactor: simplify code

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default "Message-ID": -> {
-    "<#{SecureRandom.uuid}@#{ActionMailer::Base.smtp_settings[:domain]}>"
+    "<#{SecureRandom.uuid}@#{self.smtp_settings[:domain]}>"
   }
 
   default from: "wishly-support@#{ActionMailer::Base.smtp_settings[:domain]}"
