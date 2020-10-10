f85ef78983834f069caa6e684875b81412d6c820
Ian Bayne <ian@tacchistudios.com>
2020-10-10 18:37:32 +0900

    new: add random placeholders for new item and invitee inputs

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "stimulus";
+import faker from "faker";
 
 export default class extends Controller {
   static targets = [ 'itemList', 'itemTemplate', 'inviteeList', 'inviteeTemplate' ];
@@ -13,10 +14,12 @@ export default class extends Controller {
     const itemNumber = itemList.children.length;
     const itemId = `wishlist_wishlist_items_attributes_${itemNumber}_name`
     const itemName = `wishlist[wishlist_items_attributes][${itemNumber}][name]`
+    const randomWords = faker.random.words();
 
     const itemInput = item.querySelector('input')
     itemInput.setAttribute('id', itemId);
     itemInput.setAttribute('name', itemName);
+    itemInput.setAttribute('placeholder', `New ${randomWords.toLowerCase()}`);
 
     itemList.appendChild(item);
   }
@@ -31,10 +34,12 @@ export default class extends Controller {
     const inviteeNumber = inviteeList.children.length;
     const inviteeId = `wishlist_invitees_attributes_${inviteeNumber}_email`;
     const inviteeName = `wishlist[invitees_attributes][${inviteeNumber}][email]`;
+    const randomEmail = faker.internet.email();
 
     const inviteeInput = invitee.querySelector('input');
     inviteeInput.setAttribute('id', inviteeId);
     inviteeInput.setAttribute('name', inviteeName);
+    inviteeInput.setAttribute('placeholder', randomEmail);
 
     inviteeList.appendChild(invitee);
   }
