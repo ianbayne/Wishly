5e9a62bb8da2193e201e66ab47cf48bcbb58d69d
Ian Bayne <ianbayne@gmail.com>
2022-02-28 09:07:48 +0900

    fix: disable Turbo Drive by default as was breaking forms and links

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+
+import { Turbo } from "@hotwired/turbo-rails"
+// This disables Turbo throughout the app, allowing enabling on a per element basis with `data-turbo="true"`. REF: https://github.com/hotwired/turbo-rails#navigate-with-turbo-drive
+Turbo.session.drive = false // TODO: Decide if I want to turn this on
+
 import "./controllers"
